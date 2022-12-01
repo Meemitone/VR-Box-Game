5312b7ad4141758ea752325c2876cc8eeeef1906
Meemitone <99894591+Meemitone@users.noreply.github.com>
2022-12-01 15:09:29 +0000

    Cube Started

---
+++ Assets/FaceScript.cs
@@ -0,0 +1,36 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FaceScript : MonoBehaviour
+{
+    [HideInInspector] public Vector3 inUp, inFor, inLef, inRig, inBac;
+    // Start is called before the first frame update
+    void Awake()
+    {
+        inUp = transform.up;
+        inFor = transform.forward;
+        inBac = -transform.forward;
+        inRig = transform.right;
+        inLef = -transform.right;
+    }
+
+    private void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnTriggerStay(Collider other)
+    {
+        if(other.CompareTag("Cube"))
+        {
+            gameObject.SetActive(false);
+        }
+    }
+}
