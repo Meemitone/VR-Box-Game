45f7769efe864a84f38a6ec7f58042e2b4f83861
Grumtom <Tomgrumley2002@gmail.com>
2022-12-08 15:36:32 +0000

    110 changed files

---
+++ Assets/Scripts/ColourManager.cs
@@ -0,0 +1,13 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ColourManager : MonoBehaviour
+{
+    public Material[] CurrentMaterials;
+    public Material[] StoredMaterials;
+    void Start()
+    {
+        
+    }
+}
